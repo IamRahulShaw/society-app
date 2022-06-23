e6d3a26c2639625328ff6417a0aff6377dd4c73b
Rahul Shaw <rahulramakrishna90@gmail.com>
2022-06-23 21:01:21 +0530

    error fixed

--- client/src/components/EntryQrCode.js
+++ client/src/components/EntryQrCode.js
@@ -1,62 +1,62 @@
-import React, { useContext, useRef } from "react";
-import axios from "axios";
-import { useNavigate } from "react-router-dom";
-import { QrReader } from "react-qr-reader";
-import { AuthContext } from "../App";
+// import React, { useContext, useRef } from "react";
+// import axios from "axios";
+// import { useNavigate } from "react-router-dom";
+// import { QrReader } from "react-qr-reader";
+// import { AuthContext } from "../App";
 
-function EntryQrCode() {
-  const { authData, setAuthData } = useContext(AuthContext);
-  const qrRef = useRef(null);
-  const navigate = useNavigate();
+// function EntryQrCode() {
+//   const { authData, setAuthData } = useContext(AuthContext);
+//   const qrRef = useRef(null);
+//   const navigate = useNavigate();
 
-  const onButtonClick = () => {
-    qrRef.current.openImageDialog();
-  };
+//   const onButtonClick = () => {
+//     qrRef.current.openImageDialog();
+//   };
 
-  // const onScanQr = async (result) => {
-  //   try {
-  //     if(result)
-  //     console.log("hi");
-  //     const { data } = await axios.get("/api/users/logentry", {
-  //       headers: {
-  //         Authorization: `Bearer ${localStorage.getItem("token")}`,
-  //       },
-  //     });
-  //     console.log(data);
-  //     setAuthData({
-  //       ...authData,
-  //       entries: [...(data.entries ?? null)],
-  //       exits: [...(data.exits ?? null)],
-  //     });
-  //     navigate("/logs");
-  //   } catch (e) {
-  //     navigate("/entry");
-  //   }
-  // };
+//   // const onScanQr = async (result) => {
+//   //   try {
+//   //     if(result)
+//   //     console.log("hi");
+//   //     const { data } = await axios.get("/api/users/logentry", {
+//   //       headers: {
+//   //         Authorization: `Bearer ${localStorage.getItem("token")}`,
+//   //       },
+//   //     });
+//   //     console.log(data);
+//   //     setAuthData({
+//   //       ...authData,
+//   //       entries: [...(data.entries ?? null)],
+//   //       exits: [...(data.exits ?? null)],
+//   //     });
+//   //     navigate("/logs");
+//   //   } catch (e) {
+//   //     navigate("/entry");
+//   //   }
+//   // };
 
-  const handleScanQr = (result) => {
-    console.log(result);
-  };
+//   const handleScanQr = (result) => {
+//     console.log(result);
+//   };
 
-  const handleError = (error) => {
-    console.log(error);
-  };
+//   const handleError = (error) => {
+//     console.log(error);
+//   };
 
-  return (
-    <div className="position-div-center bg-light bg-shadow ">
-      <QrReader
-        ref={qrRef}
-        onScan={handleScanQr}
-        onError={handleError}
-        style={{ width: "100%" }}
-        scanDelay={500}
-        legacyMode
-      />
-      <button className="primary-btn" onClick={onButtonClick}>
-        Upload Qr Code
-      </button>
-    </div>
-  );
-}
+//   return (
+//     <div className="position-div-center bg-light bg-shadow ">
+//       <QrReader
+//         ref={qrRef}
+//         onScan={handleScanQr}
+//         onError={handleError}
+//         style={{ width: "100%" }}
+//         scanDelay={500}
+//         legacyMode
+//       />
+//       <button className="primary-btn" onClick={onButtonClick}>
+//         Upload Qr Code
+//       </button>
+//     </div>
+//   );
+// }
 
-export default EntryQrCode;
+// export default EntryQrCode;
